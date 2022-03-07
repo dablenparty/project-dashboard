8e620d1b978abde4053cd368ed1a159bc2749812
dablenparty <hdavenport8902@gmail.com>
2022-03-06 20:51:24 -0500

    Organized project

---
+++ src/components/App/App.tsx
@@ -0,0 +1,12 @@
+import { Center } from "@mantine/core";
+import "./App.css";
+
+function App() {
+  return (
+    <Center>
+      <h1>Hello, with Mantine!</h1>
+    </Center>
+  );
+}
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/App";
+import { MantineProvider } from "@mantine/core";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MantineProvider withGlobalStyles>
+      <App />
+    </MantineProvider>
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
