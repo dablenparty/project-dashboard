91bab28f9ca50a3f332c160bb67ae738a312b361
dablenparty <hdavenport8902@gmail.com>
2022-03-07 14:04:47 -0500

    Wrapped in Provider

--- src/context/ProjectsContext.ts
+++ src/context/ProjectsContext.ts
@@ -1,38 +1,74 @@
 import { useLocalStorageValue } from "@mantine/hooks";
+import { createContext, useContext, useMemo, useState } from "react";
 import Project from "src/models/Project";
 
-const [localProjects, setLocalProjects] = useLocalStorageValue({
-  key: "projects",
-  defaultValue: "[]" as string,
-});
+const ProjectsContext = createContext<ProjectsContextInterface | undefined>(undefined);
 
-let projects: Project[] = JSON.parse(localProjects);
-
-/**
- * Adds a project to the list of projects
- *
- * @param project Project to add to the list of projects
- */
-function addProject(project: Project) {
-  projects.push(project);
-  setLocalProjects(JSON.stringify(projects));
+interface ProjectsContextInterface {
+  projects: Project[];
+  addProject: (project: Project) => void;
+  deleteProject: (projectId: string) => void;
+  editProject: (project: Project) => void;
 }
 
-/**
- * Delete a project from the list of projects
- * @param projectId ID of project to remove from the list of projects
- */
-function deleteProject(projectId: string) {
-  projects = projects.filter((p) => p.id !== projectId);
-  setLocalProjects(JSON.stringify(projects));
+export function useProjects() {
+  return useContext(ProjectsContext);
 }
 
-/**
- * Updates a project in the list of projects with the ID matching that of the given project
- * @param project Updated project to replace the existing project
- */
-function editProject(project: Project) {
-  const projectIndex = projects.findIndex((p) => p.id === project.id);
-  projects[projectIndex] = project;
-  setLocalProjects(JSON.stringify(projects));
+interface ProjectsProviderProps {
+  children: React.ReactNode;
+}
+
+export const ProjectsProvider = ({ children }: ProjectsProviderProps) => {
+  const [localProjects, setLocalProjects] = useLocalStorageValue({
+    key: "projects",
+    defaultValue: "[]" as string,
+  });
+
+  const [projects, setProjects] = useState<Project[]>(
+    JSON.parse(localProjects)
+  );
+
+  /**
+   * Adds a project to the list of projects
+   *
+   * @param project Project to add to the list of projects
+   */
+  function addProject(project: Project) {
+    setProjects([...projects, project]);
+    setLocalProjects(JSON.stringify(projects));
+  }
+
+  /**
+   * Delete a project from the list of projects
+   * @param projectId ID of project to remove from the list of projects
+   */
+  function deleteProject(projectId: string) {
+    const filtered = projects.filter((p) => p.id !== projectId);
+    setProjects(filtered);
+    setLocalProjects(JSON.stringify(projects));
+  }
+
+  /**
+   * Updates a project in the list of projects with the ID matching that of the given project
+   * @param project Updated project to replace the existing project
+   */
+  function editProject(project: Project) {
+    const projectIndex = projects.findIndex((p) => p.id === project.id);
+    const edited = [...projects];
+    edited[projectIndex] = project;
+    setProjects(edited);
+    setLocalProjects(JSON.stringify(projects));
+  }
+
+  const value = useMemo(() => {
+    return {
+      projects,
+      addProject,
+      deleteProject,
+      editProject,
+    };
+  }, [projects]);
+
+  return <ProjectsContext.Provider value={value}>{children}</ProjectsContext.Provider>;
 }
