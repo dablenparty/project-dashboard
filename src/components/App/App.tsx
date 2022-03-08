fd18ce30465e7ef24fd92bca17904ca3410c7fc0
dablenparty <hdavenport8902@gmail.com>
2022-03-07 21:19:11 -0500

    Extended form demo to show adding a project

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,34 @@
-import { Center } from "@mantine/core";
+import { Card, Center } from "@mantine/core";
 import ProjectForm from "src/components/ProjectForm";
+import { useProjects } from "src/context/ProjectsContext";
+import Project from "src/models/Project";
 import "./App.css";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
+  const projectsContext = useProjects();
   return (
     <>
       <Center>
         <h1>Hello, with Mantine!</h1>
       </Center>
-      <ProjectForm/>
+      <ProjectForm
+        title="Add Project"
+        buttonText="Add"
+        onSubmit={(values) => {
+          const project: Project = {
+            id: uuidv4(),
+            ...values,
+          };
+          projectsContext?.addProject(project);
+        }}
+      />
+      {projectsContext?.projects.map((project) => (
+        <Card>
+          <h2>{project.name}</h2>
+          <p>{project.description}</p>
+        </Card>
+      ))}
     </>
   );
 }
