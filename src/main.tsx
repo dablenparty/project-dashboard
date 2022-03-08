c8f35ffab45ec1b5e3ec7fd67fa4168c3c1c8559
dablenparty <hdavenport8902@gmail.com>
2022-03-07 21:16:07 -0500

    Wrapped App in ProjectsProvider

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 import { MantineProvider } from "@mantine/core";
+import { ProjectsProvider } from "src/context/ProjectsContext";
 
 ReactDOM.render(
   <React.StrictMode>
     <MantineProvider withGlobalStyles>
-      <App />
+      <ProjectsProvider>
+        <App />
+      </ProjectsProvider>
     </MantineProvider>
   </React.StrictMode>,
   document.getElementById("root")
