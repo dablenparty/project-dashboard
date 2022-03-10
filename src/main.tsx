76031c2344f58916d05373606ea46dfaf9817f66
dablenparty <hdavenport8902@gmail.com>
2022-03-09 19:43:24 -0500

    Fixed default import

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { render } from "react-dom";
 import "./index.css";
 import App from "./components/App";
 import { MantineProvider } from "@mantine/core";
 import { ProjectsProvider } from "./context/ProjectsContext";
 import { ModalsProvider } from "@mantine/modals";
 
-ReactDOM.render(
+render(
   <React.StrictMode>
     <MantineProvider withGlobalStyles>
       <ModalsProvider>
