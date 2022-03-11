3f78e1450ddd3cb30f67be5e6d58aaaf6bea8367
dablenparty <hdavenport8902@gmail.com>
2022-03-11 01:10:40 -0500

    Implemented searching for projects

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -11,6 +11,7 @@ import {
   UnstyledButton,
   useMantineTheme,
   Button,
+  TextInput,
 } from "@mantine/core";
 import { useState } from "react";
 import { GitHubLogoIcon, PlusIcon } from "@radix-ui/react-icons";
@@ -21,6 +22,7 @@ import { useModals } from "@mantine/modals";
 
 function App() {
   const [navbarOpened, setNavbarOpened] = useState(false);
+  const [projectSearchText, setProjectSearchText] = useState("");
   const modals = useModals();
   const theme = useMantineTheme();
   const projectsContext = useProjects();
@@ -55,26 +57,41 @@ function App() {
           width={{ sm: 300, lg: 400 }}
         >
           <Navbar.Section grow component={ScrollArea}>
-            {projectsContext?.projects.map((project) => (
-              <UnstyledButton
-                key={project.id}
-                sx={{
-                  width: "100%",
-                  padding: 10,
-                  borderRadius: 4,
-                  ":hover": { backgroundColor: theme.colors.gray[1] },
-                }}
-                onClick={() => {
-                  setSelectedProject(project);
-                  setNavbarOpened(false);
-                }}
-              >
-                <Text>{project.name}</Text>
-                <Text size={"sm"} color={theme.colors.gray[6]}>
-                  {project.description}
-                </Text>
-              </UnstyledButton>
-            ))}
+            <TextInput
+              placeholder="Search for a project"
+              value={projectSearchText}
+              onChange={(event) => setProjectSearchText(event.target.value)}
+              type={"search"}
+              mb={"xs"}
+            />
+            {projectsContext?.projects
+              .filter(
+                (p) =>
+                  p.description
+                    .toLowerCase()
+                    .includes(projectSearchText.toLowerCase()) ||
+                  p.name.toLowerCase().includes(projectSearchText.toLowerCase())
+              )
+              .map((project) => (
+                <UnstyledButton
+                  key={project.id}
+                  sx={{
+                    width: "100%",
+                    padding: 10,
+                    borderRadius: 4,
+                    ":hover": { backgroundColor: theme.colors.gray[1] },
+                  }}
+                  onClick={() => {
+                    setSelectedProject(project);
+                    setNavbarOpened(false);
+                  }}
+                >
+                  <Text>{project.name}</Text>
+                  <Text size={"sm"} color={theme.colors.gray[6]}>
+                    {project.description}
+                  </Text>
+                </UnstyledButton>
+              ))}
           </Navbar.Section>
         </Navbar>
       }
