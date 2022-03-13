2f141f37c3d94cd19538763862d7f859cb220847
dablenparty <hdavenport8902@gmail.com>
2022-03-13 14:18:54 -0400

    Extracted ProjectPage component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,55 +1,14 @@
-import {
-  AppShell,
-  Text,
-  Group,
-  Button,
-  Anchor,
-  LoadingOverlay,
-} from "@mantine/core";
+import { AppShell, Text } from "@mantine/core";
 import { useState } from "react";
-import { GitHubLogoIcon } from "@radix-ui/react-icons";
 import { useProjects } from "@context/ProjectsContext";
-import { ipcRenderer } from "electron";
-import ReactMarkdown from "react-markdown";
-import { useDidUpdate } from "@mantine/hooks";
-import useLimitedArray from "@hooks/useLimitedArray";
 import ProjectListNavbar from "@components/ProjectListNavbar";
 import AppShellHeader from "@components/AppShellHeader";
-
-interface ReadmeCacheEntry {
-  projectId: string;
-  rawText: string;
-}
+import ProjectPage from "@components/ProjectPage";
 
 function App() {
   const { projects } = useProjects();
-  const [readmeRaw, setReadmeRaw] = useState("Loading...");
   const [navbarOpened, setNavbarOpened] = useState(false);
-  const [readmeLoading, setReadmeLoading] = useState(false);
   const [selectedProject, setSelectedProject] = useState(projects[0]);
-  const { array: readmeCache, addItem: addReadmeCacheEntry } =
-    useLimitedArray<ReadmeCacheEntry>([], 5);
-
-  // This hook makes sure the function is only run once when the component is mounted
-  useDidUpdate(() => {
-    // if the selected projects README text is stored in the cache, just use that
-    const entry = readmeCache.find((e) => e.projectId == selectedProject.id);
-    if (entry) {
-      setReadmeRaw(entry.rawText);
-      return;
-    }
-    // otherwise, fetch the README text from the folder
-    setReadmeLoading(true);
-    ipcRenderer.invoke("getReadme", selectedProject.rootDir).then((readme) => {
-      const readmeText = readme ?? "No README.md found";
-      addReadmeCacheEntry({
-        projectId: selectedProject.id,
-        rawText: readmeText,
-      });
-      setReadmeRaw(readmeText);
-      setReadmeLoading(false);
-    });
-  }, [selectedProject]);
 
   // TODO: on edit list, display as list of checkbox buttons and change Add button to Delete button
   // consider adding a checkbox whose visibility is conditional on whether edit mode is enabled
@@ -78,51 +37,7 @@ function App() {
       }
     >
       {selectedProject && projects.find((p) => p.id === selectedProject.id) ? (
-        <>
-          <Group position={"apart"}>
-            <Text weight={"bold"} size={"xl"}>
-              {selectedProject.name}
-            </Text>
-            {selectedProject.url && (
-              <Button
-                onClick={async () =>
-                  await ipcRenderer.invoke("openExternal", selectedProject.url)
-                }
-                variant={"subtle"}
-                color={"gray"}
-                leftIcon={<GitHubLogoIcon />}
-              >
-                View on GitHub
-              </Button>
-            )}
-          </Group>
-          <Anchor
-            onClick={async () =>
-              await ipcRenderer.invoke("openPath", selectedProject.rootDir)
-            }
-            size={"sm"}
-            color={"dimmed"}
-          >
-            {selectedProject.rootDir}
-          </Anchor>
-          <Text mt={"md"} size={"sm"} color={"dimmed"}>
-            Description
-          </Text>
-          <Text>{selectedProject.description}</Text>
-          <div
-            style={{
-              position: "relative",
-              height: readmeLoading ? "100%" : "auto",
-            }}
-          >
-            <LoadingOverlay visible={readmeLoading} />
-            <ReactMarkdown
-              components={{ a: (props) => <a target={"_blank"} {...props} /> }}
-            >
-              {readmeRaw}
-            </ReactMarkdown>
-          </div>
-        </>
+        <ProjectPage project={selectedProject} />
       ) : (
         <Text color={"dimmed"}>No project selected</Text>
       )}
--- src/components/ProjectPage/ProjectPage.tsx
+++ src/components/ProjectPage/ProjectPage.tsx
@@ -1,5 +1,100 @@
-import React from "react";
+import useLimitedArray from "@hooks/useLimitedArray";
+import { Anchor, Button, Group, LoadingOverlay, Text } from "@mantine/core";
+import Project from "@models/Project";
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
+import { ipcRenderer } from "electron";
+import { useEffect, useState } from "react";
+import ReactMarkdown from "react-markdown";
 
-export default function ProjectPage() {
-  return <div>ProjectPage</div>;
+type ProjectPageProps = {
+  project: Project;
+};
+
+type ReadmeCacheEntry = {
+  projectId: string;
+  rawText: string;
+};
+
+/**
+ * Creates a Project Page component.
+ * 
+ * ## Important
+ * `project` should be a state variable created by the `useState` hook
+ * 
+ * @param project The project to display
+ * @returns ProjectPage component
+ */
+export default function ProjectPage({ project }: ProjectPageProps) {
+  const [readmeRaw, setReadmeRaw] = useState("Loading...");
+  const [readmeLoading, setReadmeLoading] = useState(false);
+  const { array: readmeCache, addItem: addReadmeCacheEntry } =
+    useLimitedArray<ReadmeCacheEntry>([], 5);
+
+  useEffect(() => {
+    // if the selected projects README text is stored in the cache, just use that
+    const entry = readmeCache.find((e) => e.projectId == project.id);
+    if (entry) {
+      setReadmeRaw(entry.rawText);
+      return;
+    }
+    // otherwise, fetch the README text from the folder
+    setReadmeLoading(true);
+    ipcRenderer.invoke("getReadme", project.rootDir).then((readme) => {
+      const readmeText = readme ?? "No README.md found";
+      addReadmeCacheEntry({
+        projectId: project.id,
+        rawText: readmeText,
+      });
+      setReadmeRaw(readmeText);
+      setReadmeLoading(false);
+    });
+  }, [project]);
+
+  return (
+    <>
+      <Group position={"apart"}>
+        <Text weight={"bold"} size={"xl"}>
+          {project.name}
+        </Text>
+        {project.url && (
+          <Button
+            onClick={async () =>
+              await ipcRenderer.invoke("openExternal", project.url)
+            }
+            variant={"subtle"}
+            color={"gray"}
+            leftIcon={<GitHubLogoIcon />}
+          >
+            View on GitHub
+          </Button>
+        )}
+      </Group>
+      <Anchor
+        onClick={async () =>
+          await ipcRenderer.invoke("openPath", project.rootDir)
+        }
+        size={"sm"}
+        color={"dimmed"}
+      >
+        {project.rootDir}
+      </Anchor>
+      <Text mt={"md"} size={"sm"} color={"dimmed"}>
+        Description
+      </Text>
+      <Text>{project.description}</Text>
+      <div
+        style={{
+          position: "relative",
+          height: readmeLoading ? "100%" : "auto",
+        }}
+      >
+        <LoadingOverlay visible={readmeLoading} />
+        <ReactMarkdown
+          components={{ a: (props) => <a target={"_blank"} {...props} /> }}
+        >
+          {readmeRaw}
+        </ReactMarkdown>
+      </div>
+    </>
+  );
 }
