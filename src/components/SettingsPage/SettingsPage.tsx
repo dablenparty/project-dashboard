5cfbbe71de75f5a2dc2a82409c32a00da2a2b0a9
dablenparty <hdavenport8902@gmail.com>
2022-03-14 01:28:30 -0400

    Added switch for light/dark mode

--- src/components/SettingsPage/SettingsPage.tsx
+++ src/components/SettingsPage/SettingsPage.tsx
@@ -1,5 +1,18 @@
-import React from "react";
+import { Switch, useMantineColorScheme } from "@mantine/core";
+
+function capitalize(input: string, locale = navigator.language): string {
+  //https://stackoverflow.com/questions/1026069/how-do-i-make-the-first-letter-of-a-string-uppercase-in-javascript
+  const [first, ...rest] = input;
+  return first.toLocaleUpperCase(locale) + rest.join("");
+}
 
 export default function SettingsPage() {
-  return <div>SettingsPage</div>;
+  const { colorScheme, toggleColorScheme } = useMantineColorScheme();
+
+  return (
+    <Switch
+      label={`${capitalize(colorScheme as string)} mode`}
+      onClick={() => toggleColorScheme()}
+    />
+  );
 }
