a0a45f0dd6c1db61f21e6a984402c4b872b75488
dablenparty <hdavenport8902@gmail.com>
2022-03-16 22:38:55 -0400

    Implemented SelectColorItem component

--- src/components/SelectColorItem/SelectColorItem.tsx
+++ src/components/SelectColorItem/SelectColorItem.tsx
@@ -1,5 +1,23 @@
-import React from "react";
+import { ColorSwatch, Group, Text, useMantineTheme } from "@mantine/core";
+import { forwardRef } from "react";
 
-export default function SelectColorItem() {
-  return <div>SelectColorItem</div>;
+interface SelectColorItemProps {
+  color: string;
+  label: string;
 }
+
+const SelectColorItem = forwardRef<HTMLDivElement, SelectColorItemProps>(
+  ({ color, label, ...others }, ref) => {
+    const theme = useMantineTheme();
+    return (
+      <div ref={ref} {...others}>
+        <Group noWrap>
+          <ColorSwatch key={color} color={theme.colors[color][6]} />
+          <Text size={"sm"}>{label}</Text>
+        </Group>
+      </div>
+    );
+  }
+);
+
+export default SelectColorItem;
