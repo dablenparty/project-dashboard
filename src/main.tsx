61d351ed62574295949c3716d64bc5886d52e6a8
dablenparty <hdavenport8902@gmail.com>
2022-04-01 16:08:22 -0400

    Fixed lint error

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import React from "react";
+import { StrictMode } from "react";
 import { render } from "react-dom";
 import "./index.css";
 import App from "@components/App";
 
 render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>,
+  </StrictMode>,
   document.getElementById("root")
 );
