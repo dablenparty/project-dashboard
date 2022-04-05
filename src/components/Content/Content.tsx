e36572edce7d33f921f5757eee784c630717db27
dablenparty <hdavenport8902@gmail.com>
2022-04-05 14:19:16 -0400

    Removed unnecessary state from Content

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,6 +1,5 @@
 import { AppShell, Text } from "@mantine/core";
 import { useState } from "react";
-import { useProjects } from "@context/ProjectsContext";
 import ProjectListNavbar from "@components/ProjectListNavbar";
 import AppShellHeader from "@components/AppShellHeader";
 import ProjectPage from "@components/ProjectPage";
@@ -15,11 +14,10 @@ type ReadmeCacheEntry = {
 };
 
 function Content() {
-  const { projects } = useProjects();
   const [readmeRaw, setReadmeRaw] = useState<string>("Loading...");
   const [navbarOpened, setNavbarOpened] = useState(false);
   const [selectedProject, setSelectedProject] = useState<Project | undefined>(
-    projects[0]
+    undefined
   );
   const { array: readmeCache, addItem: addReadmeCacheEntry } =
     useLimitedArray<ReadmeCacheEntry>([], 5);
