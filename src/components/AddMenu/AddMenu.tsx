a58126cfb0e56e91f6832b4a03c7dbec3af07459
dablenparty <hdavenport8902@gmail.com>
2022-04-07 20:37:26 -0400

    Added handlers for buttons

--- src/components/AddMenu/AddMenu.tsx
+++ src/components/AddMenu/AddMenu.tsx
@@ -1,5 +1,23 @@
-export default function AddMenu() {
+import { Menu } from "@mantine/core";
+import { ChevronUpIcon, DoubleArrowUpIcon } from "@radix-ui/react-icons";
+
+type AddMenuProps = {
+  onAddOneClick?: () => void;
+  onAddManyClick?: () => void;
+};
+
+export default function AddMenu({
+  onAddOneClick,
+  onAddManyClick,
+}: AddMenuProps) {
   return (
-    <div>AddMenu</div>
-  )
+    <Menu placement="end">
+      <Menu.Item onClick={onAddOneClick} icon={<ChevronUpIcon />}>
+        Add one
+      </Menu.Item>
+      <Menu.Item onClick={onAddManyClick} icon={<DoubleArrowUpIcon />}>
+        Add many
+      </Menu.Item>
+    </Menu>
+  );
 }
