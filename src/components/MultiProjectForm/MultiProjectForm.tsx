c93065c103bf5d0c977e6a4d7a6a564614a5c321
dablenparty <hdavenport8902@gmail.com>
2022-04-07 21:35:07 -0400

    Created form skeleton

--- src/components/MultiProjectForm/MultiProjectForm.tsx
+++ src/components/MultiProjectForm/MultiProjectForm.tsx
@@ -1,5 +1,16 @@
+import { Button, Group, Paper, Text } from "@mantine/core";
+
 export default function MultiProjectForm() {
   return (
-    <div>MultiProjectForm</div>
-  )
+    <Paper>
+      <form>
+        <Group direction={"column"} spacing={0}>
+          <Text>
+            Folders<span style={{ color: "#f03e3e" }}>*</span>
+          </Text>
+          <Button>Select</Button>
+        </Group>
+      </form>
+    </Paper>
+  );
 }
