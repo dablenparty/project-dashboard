ea5ec952f0d97e06246f9f561e1457dcc42a26b6
dablenparty <hdavenport8902@gmail.com>
2022-04-08 16:49:41 -0400

    Displays selected projects

--- src/components/MultiProjectForm/MultiProjectForm.tsx
+++ src/components/MultiProjectForm/MultiProjectForm.tsx
@@ -1,6 +1,29 @@
-import { Button, Group, Paper, Text } from "@mantine/core";
+import {
+  ActionIcon,
+  Button,
+  Group,
+  Paper,
+  Text,
+  TextInput,
+  useMantineTheme,
+} from "@mantine/core";
+import { useListState } from "@mantine/hooks";
+import { Cross2Icon } from "@radix-ui/react-icons";
+
+type BulkProject = {
+  name: string;
+  description: string;
+  rootDir: string;
+};
 
 export default function MultiProjectForm() {
+  const theme = useMantineTheme();
+  const [projects, projectsHandlers] = useListState<BulkProject>();
+
+  const selectDirectories = () => {
+    console.log("select directories");
+  };
+
   return (
     <Paper>
       <form>
@@ -8,7 +31,27 @@ export default function MultiProjectForm() {
           <Text>
             Folders<span style={{ color: "#f03e3e" }}>*</span>
           </Text>
-          <Button>Select</Button>
+          <Button onClick={selectDirectories}>Select</Button>
+        </Group>
+        <Group mt={"md"} direction={"column"}>
+          {projects.map((project, index) => (
+            <Paper
+              p={"sm"}
+              sx={{ width: "100%", backgroundColor: theme.colors.dark[6] }}
+              key={project.rootDir}
+              shadow={"md"}
+              withBorder
+            >
+              <Group mb={"sm"} position="apart">
+                <Text>{project.rootDir}</Text>
+                <ActionIcon onClick={() => projectsHandlers.remove(index)}>
+                  <Cross2Icon />
+                </ActionIcon>
+              </Group>
+              <TextInput defaultValue={project.name} />
+              <TextInput mt={"sm"} defaultValue={project.description} />
+            </Paper>
+          ))}
         </Group>
       </form>
     </Paper>
